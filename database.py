d88429f6e0e98bae0e9ad511e6608535f8ef36ca
Yeferson Gallo <yefersongallocastro@gmail.com>
2021-06-29 16:19:33 -0500

    Se agrega commit en las peticiones a la BD

--- database.py
+++ database.py
@@ -11,45 +11,52 @@ class Connection:
     print(
         "Opened database successfully")
 
-    cur = conn.cursor()
-
     def get_population_by_city(self, name):
-        self.cur.execute("SELECT population, trim(name) from location WHERE name=$$" + name + "$$")
-        rows = self.cur.fetchall()
+        cur = self.conn.cursor()
+        cur.execute("SELECT population, trim(name) from location WHERE name=$$" + name + "$$")
+        rows = cur.fetchall()
         names = {}
         for row in rows:
             names["name"] = row[0]
             names["population"] = row[1]
+        self.conn.commit()
+        cur.close()
         return names
 
     def get_population_by_state(self, name, country=None):
+        cur = self.conn.cursor()
         if country:
-            self.cur.execute(
+            cur.execute(
                 "SELECT id_state from location WHERE name = $$" + name + "$$ AND country_id = " + country + "")
         else:
-            self.cur.execute(
+            cur.execute(
                 "SELECT id_state from location WHERE name = $$" + name + "$$")
-        rows = self.cur.fetchall()
+        rows = cur.fetchall()
         state = []
         names = {}
         for row in rows:
             state.append(row[0])
-        self.cur.execute("SELECT trim(name), population from location WHERE state_id=" + str(state[0]))
-        rows_names = self.cur.fetchall()
+        cur.execute("SELECT trim(name), population from location WHERE state_id=" + str(state[0]))
+        rows_names = cur.fetchall()
         for i, row in enumerate(rows_names):
             names["city_" + str(i)] = {"name": row[0], "population": row[1]}
+        self.conn.commit()
+        cur.close()
         return names
 
     def get_population_by_country(self, name):
-        self.cur.execute("SELECT id_country from location WHERE name=$$" + name + "$$")
-        rows = self.cur.fetchall()
+        cur = self.conn.cursor()
+        cur.execute("SELECT id_country from location WHERE name=$$" + name + "$$")
+        rows = cur.fetchall()
         country = []
         names = {}
         for row in rows:
             country.append(row[0])
-        self.cur.execute("SELECT trim(name), id_state from location WHERE country_id=" + str(country[0]))
-        rows_names = self.cur.fetchall()
+        cur.execute("SELECT trim(name), id_state from location WHERE country_id=" + str(country[0]))
+        rows_names = cur.fetchall()
         for i, row in enumerate(rows_names):
             names["state_" + str(i)] = {"state": str(row[0]),
                                         "cities": self.get_population_by_state(str(row[0]), str(country[0]))}
+        self.conn.commit()
+        cur.close()
         return names
